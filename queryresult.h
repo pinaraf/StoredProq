39d4373109785c967dbe1182387efa58153507f1
Pierre Ducroquet <pinaraf@pinaraf.info>
2014-12-29 21:06:40 +0100

    Code simplification

--- queryresult.h
+++ queryresult.h
@@ -82,6 +82,51 @@ public:
     }
 };
 
+
+template <typename T>
+class SqlQueryResultMapper<QList<T>>
+{
+public:
+    template <typename R = typename std::remove_pointer<T>::type>
+    typename std::enable_if<std::is_base_of<QObject, R>::value, QList<R*>>::type
+    map(QSqlQuery *query)
+    {
+        QList<R*> resultList;
+        //Q_ASSERT(query->record().count() == 1);
+        while (query->next())
+        {
+            T result = new R();
+            QSqlRecord rec = query->record();
+            mapRecordToQObject(rec, result);
+            resultList << result;
+        }
+        return resultList;
+    }
+
+    template <typename R = typename std::remove_pointer<T>::type>
+    typename std::enable_if<!std::is_base_of<QObject, R>::value, QList<R>>::type
+    map(QSqlQuery *query)
+    {
+        QList<R> resultList;
+        while (query->next())
+        {
+            QSqlRecord rec = query->record();
+            resultList << mapRecordFieldToValue<R>(rec, 0);
+        }
+        return resultList;
+    }
+};
+
+template <>
+class SqlQueryResultMapper<void>
+{
+public:
+    void map(QSqlQuery *query)
+    {
+        query->next();
+    }
+};
+
 template <typename ...Args>
 class SqlQueryResultMapper<std::tuple<Args...>>
 {
@@ -111,72 +156,4 @@ public:
     }
 };
 
-template <>
-class SqlQueryResultMapper<QList<QDateTime>>
-{
-public:
-    QList<QDateTime> map(QSqlQuery *query)
-    {
-        QList<QDateTime> result;
-        //Q_ASSERT(query->record().count() == 1);
-        while (query->next())
-        {
-            result << query->value(0).toDateTime();
-        }
-        return result;
-    }
-};
-
-template <>
-class SqlQueryResultMapper<void>
-{
-public:
-    void map(QSqlQuery *query)
-    {
-        query->next();
-    }
-};
-
-template <>
-class SqlQueryResultMapper<QList<int>>
-{
-public:
-    QList<int> map(QSqlQuery *query)
-    {
-        QList<int> result;
-        //Q_ASSERT(query->record().count() == 1);
-        while (query->next())
-        {
-            result << query->value(0).toInt();
-        }
-        return result;
-    }
-};
-
-template <typename T>
-class SqlQueryResultMapper<QList<T*>>
-{
-public:
-    SqlQueryResultMapper() {
-        metaObject = T::staticMetaObject;
-    }
-
-    QList<T*> map(QSqlQuery *query)
-    {
-        QList<T*> resultList;
-        while (query->next()) {
-            T *result = new T();
-            QSqlRecord rec = query->record();
-
-            mapRecordToQObject(rec, result);
-            resultList.append(result);
-        }
-        return resultList;
-    }
-
-private:
-    QMetaObject metaObject;
-};
-
 #endif // QUERYRESULT_H
-

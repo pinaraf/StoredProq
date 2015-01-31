260a919a8b5d350ad9c3277d2f6625a8c2b02cbb
Pierre Ducroquet <pinaraf@pinaraf.info>
2015-01-31 14:19:54 +0100

    Add headers licenses

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,29 @@
+/*
+ * This file is part of the StoredProq project
+ * distributed under the MIT License (MIT)
+ *
+ * Copyright (c) 2015 Pierre Ducroquet <pinaraf@pinaraf.info>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
 #include <QCoreApplication>
 #include "operation.h"
 #include <QMetaProperty>
@@ -12,18 +38,24 @@ int main(int, char *[])
     QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
     db.setHostName("localhost");
     db.setDatabaseName("test");
-    db.setUserName("snoopy");
-    db.setPassword("snoopy");
+    db.setUserName("moi");
+    db.setPassword("moi");
     bool ok = db.open();
     if (!ok)
         qFatal("Could not open db");
     else
         qDebug() << "DB opened";
 
+    QVector<QString> data;
+    data << "Hello world";
+    data << "Hello ' '' ''' world !";
+    qDebug() << "data:" << data;
+    SqlBindingMapper<QList<QString>, QVector<QString>> unnest("unnest");
+    qDebug() << unnest(data);
+
     SqlBindingMapper<int, std::tuple<int, QString>> testCard("test_card");
     int value = testCard(std::make_tuple(1, "test"));
     qDebug() << value;
-
 
     SqlBindingMapper<int, QVector<int>, int> array_length("array_length");
     QVector<int> data;
@@ -70,6 +102,7 @@ int main(int, char *[])
     QJsonDocument doc = QJsonDocument::fromJson("{\"hello\": {\"world\": false, \"me\": true}}");
     SqlBindingMapper<QJsonDocument, QJsonDocument, QString> json_extractor("test_json");
     qDebug() << json_extractor(doc, "{hello}").toJson();
+
 #if 0
     SqlBindingMapper<int, std::tuple<int, int>> summer("sum_me");
     qDebug() << summer(std::make_tuple(1, 2));
